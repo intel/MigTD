e83a81008099ce1eeacaa6e9ccfa6ac2c703b3b1
Jiaqi Gao <jiaqi.gao@intel.com>
2023-12-12 08:40:07 -0500

    tools: introduce `servtd-hash`
    `sertd-hash` is used to calculate `TEE_INFO_HASH` of migtd using
    migtd image and configuration manifest.
    
    `td-shim/td-shim-tools` is used to calculate mrtd and rtmr[0-1]
    and extended to calculate the rtmr2 which is used by migtd.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

---
+++ tools/migtd-hash/src/lib.rs
@@ -0,0 +1,94 @@
+// Copyright (c) 2023 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+use anyhow::{anyhow, Error, Result};
+use crypto::{hash::digest_sha384, SHA384_DIGEST_SIZE};
+use migtd::config::{CONFIG_VOLUME_SIZE, MIGTD_POLICY_FFS_GUID, MIGTD_ROOT_CA_FFS_GUID};
+use std::{
+    fs::File,
+    io::{Read, Seek, SeekFrom},
+    mem::size_of,
+};
+use td_shim_tools::tee_info_hash::{Manifest, TdInfoStruct};
+use td_uefi_pi::{fv, pi};
+
+const MIGTD_IMAGE_SIZE: u64 = 0x100_0000;
+
+pub fn calculate_servtd_hash(manifest: &[u8], mut image: File) -> Result<Vec<u8>, Error> {
+    // Initialize the configurable fields of TD info structure.
+    let manifest = serde_json::from_slice::<Manifest>(&manifest)?;
+    let mut td_info = TdInfoStruct {
+        attributes: manifest.attributes,
+        xfam: manifest.xfam,
+        mrconfig_id: manifest.mrconfigid,
+        mrowner: manifest.mrowner,
+        mrownerconfig: manifest.mrownerconfig,
+        ..Default::default()
+    };
+
+    // Calculate the MRTD with MigTD image
+    td_info.build_mrtd(&mut image, MIGTD_IMAGE_SIZE);
+    // Calculate RTMR0 and RTMR1
+    td_info.build_rtmr_with_seperator(0);
+    // Calculate RTMR2 with CFV
+    let mut cfv = vec![0u8; CONFIG_VOLUME_SIZE];
+    image.seek(SeekFrom::Start(0))?;
+    image.read(&mut cfv)?;
+    td_info.rtmr2.copy_from_slice(rtmr2(&cfv)?.as_slice());
+
+    // Convert the TD info structure to bytes.
+    let mut buffer = [0u8; size_of::<TdInfoStruct>()];
+    td_info.pack(&mut buffer);
+
+    // Calculate digest.
+    digest_sha384(&buffer).map_err(|_| anyhow!("Calculate digest"))
+}
+
+fn rtmr2(cfv: &[u8]) -> Result<Vec<u8>, Error> {
+    let policy = fv::get_file_from_fv(cfv, pi::fv::FV_FILETYPE_RAW, MIGTD_POLICY_FFS_GUID)
+        .ok_or(anyhow!("Unable to get policy from image"))?;
+    let root_ca = fv::get_file_from_fv(cfv, pi::fv::FV_FILETYPE_RAW, MIGTD_ROOT_CA_FFS_GUID)
+        .ok_or(anyhow!("Unable to get root CA from image"))?;
+
+    let mut rtmr2 = Rtmr::new();
+    rtmr2.extend_with_raw_data(policy)?;
+    rtmr2.extend_with_raw_data(root_ca)?;
+
+    Ok(rtmr2.as_bytes().to_vec())
+}
+
+struct Rtmr {
+    reg: [u8; SHA384_DIGEST_SIZE * 2],
+}
+
+impl Rtmr {
+    fn new() -> Self {
+        Self {
+            reg: [0u8; SHA384_DIGEST_SIZE * 2],
+        }
+    }
+
+    fn extend_with_raw_data(&mut self, data: &[u8]) -> Result<(), Error> {
+        let digest = calculate_digest(data)?;
+
+        self.reg[SHA384_DIGEST_SIZE..].copy_from_slice(&digest);
+        let digest = calculate_digest(&self.reg)?;
+        self.reg[..SHA384_DIGEST_SIZE].copy_from_slice(&digest);
+
+        Ok(())
+    }
+
+    fn as_bytes(&self) -> &[u8] {
+        &self.reg[..SHA384_DIGEST_SIZE]
+    }
+}
+
+fn calculate_digest(data: &[u8]) -> Result<Vec<u8>, Error> {
+    let digest = digest_sha384(data).map_err(|_| anyhow!("Calculate digest"))?;
+    if digest.len() != SHA384_DIGEST_SIZE {
+        return Err(anyhow!("Calculate digest"));
+    }
+
+    Ok(digest)
+}
---
+++ tools/migtd-hash/src/main.rs
@@ -0,0 +1,56 @@
+// Copyright (c) 2023 Intel Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+use clap::Parser;
+use migtd_hash::calculate_servtd_hash;
+use std::{
+    fs::{self, File},
+    path::PathBuf,
+    process::exit,
+};
+
+#[derive(Clone, Parser)]
+struct Config {
+    /// A json format manifest that contains values of TD info fields
+    #[clap(short, long)]
+    pub manifest: String,
+    /// Path of MigTD image file
+    #[clap(short, long)]
+    pub image: String,
+    /// Output binary of tee info hash
+    #[clap(short, long)]
+    pub output_file: Option<PathBuf>,
+}
+
+fn main() {
+    let config = Config::parse();
+
+    let image = File::open(config.image).unwrap_or_else(|e| {
+        eprintln!("Failed to open MigTD image: {}", e);
+        exit(1);
+    });
+    let manifest = fs::read(config.manifest).unwrap_or_else(|e| {
+        eprintln!("Failed to open manifest file: {}", e);
+        exit(1);
+    });
+
+    let hash = calculate_servtd_hash(&manifest, image).unwrap_or_else(|e| {
+        eprintln!("Failed to calculate hash: {:?}", e);
+        exit(1);
+    });
+
+    if let Some(output_file) = config.output_file {
+        fs::write(output_file, &hash).unwrap_or_else(|e| {
+            eprintln!("Failed to write output file: {}", e);
+            exit(1);
+        })
+    } else {
+        println!(
+            "{}",
+            hash.iter()
+                .map(|byte| format!("{:02x}", byte))
+                .collect::<String>()
+        )
+    }
+}
