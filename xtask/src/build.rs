caf252e322dfbe1cab5fd87d8fb7de00828f17cb
Jiaqi Gao <jiaqi.gao@intel.com>
2024-03-06 08:50:33 -0500

    xtask: always build `td-shim` binary with release profile
    Enable debug profile for migtd binary only. This can minimize the
    size of the `BFV` region.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- xtask/src/build.rs
+++ xtask/src/build.rs
@@ -126,13 +126,10 @@ impl BuildArgs {
 
         let sh = Shell::new()?;
         sh.change_dir(SHIM_FOLDER.as_path());
-        cmd!(sh, "cargo xbuild -p td-shim --target x86_64-unknown-none --features=main,tdx --no-default-features")
-            .args(["--profile", self.profile()])
+        cmd!(sh, "cargo xbuild -p td-shim --target x86_64-unknown-none --features=main,tdx --no-default-features --release")
 			.run()?;
 
-        let shim_output = SHIM_FOLDER
-            .join("target/x86_64-unknown-none/")
-            .join(&self.profile_path());
+        let shim_output = SHIM_FOLDER.join("target/x86_64-unknown-none/release");
 
         Ok((
             shim_output.join("ResetVector.bin"),
