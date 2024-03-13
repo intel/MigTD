cc24b23384e0a3e1dcd2ad328b2c52238468b4ef
Jiaqi Gao <jiaqi.gao@intel.com>
2024-03-13 10:48:39 -0400

    policy: log detail error info in policy crate
    Clippy reports the warnings that the Err-variant returned `PolicyError`
    is very large.
    
    One solution is to use `Box` to hold the error information but it is
    not prefered because we need to minimize the use of heap.
    
    Another solution is to reduce the size of structure. By logging the
    error information inside `policy` crate, the detailed info can be
    removed to make the size of `PolicyError` much smaller.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- src/policy/src/lib.rs
+++ src/policy/src/lib.rs
@@ -8,7 +8,7 @@ extern crate alloc;
 mod config;
 mod verify;
 
-use alloc::{format, string::String};
+use alloc::string::String;
 pub use config::*;
 pub use verify::*;
 
@@ -21,39 +21,9 @@ pub enum PolicyError {
     InvalidEventLog,
     PlatformNotFound(String),
     PlatformNotMatch(String, String),
-    UnqulifiedPlatformInfo(PolicyErrorDetails),
-    UnqulifiedQeInfo(PolicyErrorDetails),
-    UnqulifiedTdxModuleInfo(PolicyErrorDetails),
-    UnqulifiedMigTdInfo(PolicyErrorDetails),
+    UnqulifiedPlatformInfo,
+    UnqulifiedQeInfo,
+    UnqulifiedTdxModuleInfo,
+    UnqulifiedMigTdInfo,
     Crypto,
-}
-
-#[derive(Debug)]
-pub struct PolicyErrorDetails {
-    pub property: String,
-    pub policy: Property,
-    pub local_fmspc: Option<String>,
-    pub remote_fmspc: Option<String>,
-    pub local: String,
-    pub remote: String,
-}
-
-impl PolicyErrorDetails {
-    pub(crate) fn new(
-        property: String,
-        policy: Property,
-        local_fmspc: Option<String>,
-        remote_fmspc: Option<String>,
-        local: &[u8],
-        remote: &[u8],
-    ) -> Self {
-        Self {
-            property,
-            policy,
-            local: format!("{:x?}", local),
-            remote: format!("{:x?}", remote),
-            local_fmspc,
-            remote_fmspc,
-        }
-    }
 }
