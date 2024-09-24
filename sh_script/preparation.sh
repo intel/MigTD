08b1bbade6dcfa279506d2bc7fe143114a895407
Jiaqi Gao <jiaqi.gao@intel.com>
2024-09-24 09:22:37 -0400

    remove rustls git module and patch
    As we no longer need to maintain the patches for rustls, remove the
    submodule and patch file.
    
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- sh_script/preparation.sh
+++ sh_script/preparation.sh
@@ -4,12 +4,6 @@ preparation() {
     pushd deps/td-shim
     bash sh_script/preparation.sh
     popd
-
-    pushd deps/rustls
-    git reset --hard ae277befb5061bbd4c44fea1c2697f2da5b2f6fa
-    git clean -f -d
-    patch -p 1 -i ../patches/rustls.diff
-    popd
 }
 
 preparation
