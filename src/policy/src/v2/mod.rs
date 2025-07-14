d940645721b38800f368bec0c0de1250d43d5261
Jiaqi Gao <jiaqi.gao@intel.com>
2025-07-14 04:57:25 -0400

    policy: support v2 policy
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- src/policy/src/v2/mod.rs
+++ src/policy/src/v2/mod.rs
@@ -5,6 +5,9 @@
 use crate::{CcEvent, EventName, PolicyError};
 use alloc::{collections::btree_map::BTreeMap, format, string::String, vec::Vec};
 
+mod policy;
+pub use policy::*;
+
 // Verify the hash of a specific event in the event log
 fn verify_event_hash(
     events: &BTreeMap<EventName, CcEvent>,
