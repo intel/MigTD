bc1b7b822ca977a63ab458ae3a56579538cf3ab5
Jiaqi Gao <jiaqi.gao@intel.com>
2025-09-09 11:19:43 -0400

    attestation: support `verify_quote_with_collaterals`
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- src/attestation/src/attest.rs
+++ src/attestation/src/attest.rs
@@ -2,17 +2,63 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 
+#[cfg(feature = "attest-lib-ext")]
+use crate::binding::verify_quote_integrity_ex;
 use crate::{
-    binding::get_quote as get_quote_inner, binding::init_heap, binding::verify_quote_integrity,
-    binding::AttestLibError, root_ca::ROOT_CA, Error, TD_VERIFIED_REPORT_SIZE,
+    binding::{
+        get_quote as get_quote_inner, init_heap, verify_quote_integrity, AttestLibError,
+        QveCollateral,
+    },
+    root_ca::ROOT_CA,
+    Error, TD_VERIFIED_REPORT_SIZE,
 };
-use alloc::{vec, vec::Vec};
+use alloc::{ffi::CString, vec, vec::Vec};
 use core::{alloc::Layout, ffi::c_void, ops::Range};
 use tdx_tdcall::tdreport::*;
 
 const TD_QUOTE_SIZE: usize = 0x2000;
 const TD_REPORT_VERIFY_SIZE: usize = 1024;
 const ATTEST_HEAP_SIZE: usize = 0x80000;
+
+/// C-compatible version of Collateral with null-terminated strings
+#[derive(Debug)]
+pub struct Collateral {
+    pub major_version: u16,
+    pub minor_version: u16,
+    pub tee_type: u32,
+    pub pck_crl_issuer_chain: CString,
+    pub root_ca_crl: CString,
+    pub pck_crl: CString,
+    pub tcb_info_issuer_chain: CString,
+    pub tcb_info: CString,
+    pub qe_identity_issuer_chain: CString,
+    pub qe_identity: CString,
+}
+
+impl Into<QveCollateral> for &Collateral {
+    fn into(self) -> QveCollateral {
+        QveCollateral {
+            major_version: self.major_version,
+            minor_version: self.minor_version,
+            tee_type: self.tee_type,
+            pck_crl_issuer_chain: self.pck_crl_issuer_chain.as_ptr(),
+            pck_crl_issuer_chain_size: self.pck_crl_issuer_chain.as_bytes_with_nul().len() as u32,
+            root_ca_crl: self.root_ca_crl.as_ptr(),
+            root_ca_crl_size: self.root_ca_crl.as_bytes_with_nul().len() as u32,
+            pck_crl: self.pck_crl.as_ptr(),
+            pck_crl_size: self.pck_crl.as_bytes_with_nul().len() as u32,
+            tcb_info_issuer_chain: self.tcb_info_issuer_chain.as_ptr(),
+            tcb_info_issuer_chain_size: self.tcb_info_issuer_chain.as_bytes_with_nul().len() as u32,
+            tcb_info: self.tcb_info.as_ptr(),
+            tcb_info_size: self.tcb_info.as_bytes_with_nul().len() as u32,
+            qe_identity_issuer_chain: self.qe_identity_issuer_chain.as_ptr(),
+            qe_identity_issuer_chain_size: self.qe_identity_issuer_chain.as_bytes_with_nul().len()
+                as u32,
+            qe_identity: self.qe_identity.as_ptr(),
+            qe_identity_size: self.qe_identity.as_bytes_with_nul().len() as u32,
+        }
+    }
+}
 
 pub fn attest_init_heap() -> Option<usize> {
     unsafe {
@@ -74,6 +120,45 @@ pub fn verify_quote(quote: &[u8]) -> Result<Vec<u8>, Error> {
 
     if report_verify_size as usize != TD_VERIFIED_REPORT_SIZE {
         return Err(Error::InvalidOutput);
+    }
+
+    mask_verified_report_values(&mut td_report_verify[..report_verify_size as usize]);
+    Ok(td_report_verify[..report_verify_size as usize].to_vec())
+}
+
+#[cfg(feature = "attest-lib-ext")]
+pub fn verify_quote_with_collaterals(
+    quote: &[u8],
+    collateral: Collateral,
+) -> Result<Vec<u8>, Error> {
+    let mut td_report_verify = vec![0u8; TD_REPORT_VERIFY_SIZE];
+    let mut report_verify_size = TD_REPORT_VERIFY_SIZE as u32;
+
+    // Safety:
+    // ROOT_CA must have been set and checked at this moment.
+    let public_key = ROOT_CA
+        .get()
+        .unwrap()
+        .tbs_certificate
+        .subject_public_key_info
+        .subject_public_key
+        .as_bytes()
+        .unwrap();
+
+    let qve_collateral: QveCollateral = (&collateral).into();
+    unsafe {
+        let result = verify_quote_integrity_ex(
+            quote.as_ptr() as *const c_void,
+            quote.len() as u32,
+            public_key.as_ptr() as *const c_void,
+            public_key.len() as u32,
+            &qve_collateral as *const QveCollateral,
+            td_report_verify.as_mut_ptr() as *mut c_void,
+            &mut report_verify_size as *mut u32,
+        );
+        if result != AttestLibError::Success {
+            return Err(Error::VerifyQuote);
+        }
     }
 
     mask_verified_report_values(&mut td_report_verify[..report_verify_size as usize]);
