76dcbf63e76e261fb3fb586846d41a7e93ee7ff3
Jiaqi Gao <jiaqi.gao@intel.com>
2025-09-09 11:08:13 -0400

    migtd: verify the signed policy before use it
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- src/migtd/src/config.rs
+++ src/migtd/src/config.rs
@@ -26,6 +26,16 @@ pub const MIGTD_ROOT_CA_FFS_GUID: Guid = Guid::from_fields(
     &[0xA2, 0x1B, 0xD0, 0xC8, 0xFF, 0xF6],
 );
 
+// {B3C1DCFE-6BEF-449F-A183-63A84EA1E0B4}
+pub const MIGTD_POLICY_ISSUER_CHAIN_FFS_GUID: Guid = Guid::from_fields(
+    0xb3c1dcfe,
+    0x6bef,
+    0x449f,
+    0xa1,
+    0x83,
+    &[0x63, 0xa8, 0x4e, 0xa1, 0xe0, 0xb4],
+);
+
 pub fn get_config_volume() -> &'static [u8] {
     unsafe { core::slice::from_raw_parts(CONFIG_VOLUME_BASE as *const u8, CONFIG_VOLUME_SIZE) }
 }
@@ -38,4 +48,13 @@ pub fn get_policy() -> Option<&'static [u8]> {
 pub fn get_root_ca() -> Option<&'static [u8]> {
     let cfv = get_config_volume();
     fv::get_file_from_fv(cfv, pi::fv::FV_FILETYPE_RAW, MIGTD_ROOT_CA_FFS_GUID)
+}
+
+pub fn get_policy_issuer_chain() -> Option<&'static [u8]> {
+    let cfv = get_config_volume();
+    fv::get_file_from_fv(
+        cfv,
+        pi::fv::FV_FILETYPE_RAW,
+        MIGTD_POLICY_ISSUER_CHAIN_FFS_GUID,
+    )
 }
