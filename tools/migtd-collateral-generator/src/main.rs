f66a1bdc133eace00cf71411b50a86dd08a3a484
Haitao Huang <haitaohuang@microsoft.com>
2025-11-16 18:14:51 -0800

    tools/migtd-collateral-generator: support cached collaterals
    For solutions with cached collaterals such as in most CSP cases,
    collaterals in v2 policies need to be in sync with the cached
    collaterals on the particular cloud platform to ensure verification
    success.
    
    The migtd-collateral-generator currently only downloads collaterals from
    Intel PCS. This change is to support fetching cached collaterals for
    policy generation from caches by different cloud providers, abstract the
    endpoints into a more generic PcsConfig trait. Implemented for Intel
    PCS as the base config, allowing cloud specific extension later.
    
    Signed-off-by: Haitao Huang <haitaohuang@microsoft.com>

--- tools/migtd-collateral-generator/src/main.rs
+++ tools/migtd-collateral-generator/src/main.rs
@@ -3,24 +3,39 @@
 // SPDX-License-Identifier: BSD-2-Clause-Patent
 
 use clap::Parser;
-use migtd_collateral_generator::generate_collaterals;
+use migtd_collateral_generator::{generate_collaterals, IntelPcsConfig};
 use std::{path::PathBuf, process::exit};
 
 #[derive(Debug, Clone, Parser)]
 struct Config {
-    /// Set to use pre-prodution server. Production server is used by
-    /// default.
+    /// Service provider: "intel" (default) or other
+    #[clap(long, default_value = "intel")]
+    provider: String,
+
+    /// Set to use pre-production server.
     #[clap(long)]
-    pub pre_production: bool,
+    pre_production: bool,
+
     /// Where to write the generated collaterals
     #[clap(long, short)]
-    pub output: PathBuf,
+    output: PathBuf,
 }
 
 fn main() {
     let config = Config::parse();
 
-    if let Err(e) = generate_collaterals(!config.pre_production, &config.output) {
+    let result = match config.provider.to_lowercase().as_str() {
+        "intel" => {
+            let pcs_config = IntelPcsConfig::new(!config.pre_production);
+            generate_collaterals(&pcs_config, &config.output)
+        }
+        _ => {
+            eprintln!("Error: Invalid provider: {}", config.provider);
+            exit(1);
+        }
+    };
+
+    if let Err(e) = result {
         eprintln!("Error generating collaterals: {}", e);
         exit(1);
     }
