7e200449fb333f3333053bccedebf30c65fc8179
Jiaqi Gao <jiaqi.gao@intel.com>
2025-11-21 17:37:49 +0800

    migtd: refactor pre-session data implementation to align with spec
    Signed-off-by: Jiaqi Gao <jiaqi.gao@intel.com>

--- src/migtd/src/migration/session.rs
+++ src/migtd/src/migration/session.rs
@@ -633,16 +633,17 @@ pub fn report_status(status: u8, request_id: u64) -> Result<()> {
 
 #[cfg(feature = "policy_v2")]
 #[repr(C)]
-struct MigtdMessageHeader {
+struct PreSessionMessage {
     pub r#type: u8,
     pub reserved: [u8; 3],
-    pub length: u32, // Length of the command data
+    pub length: u32, // Length in bytes of the message payload
 }
 
 #[cfg(feature = "policy_v2")]
-impl MigtdMessageHeader {
-    const EXCHANGE_POLICY_TYPE: u8 = 1;
+impl PreSessionMessage {
+    const PRE_SESSION_DATA_TYPE: u8 = 1;
     const START_SESSION_TYPE: u8 = 2;
+    const HELLO_PACKET_TYPE: u8 = 0xff;
 
     pub fn as_bytes(&self) -> &[u8] {
         unsafe { core::slice::from_raw_parts(self as *const Self as *const u8, size_of::<Self>()) }
@@ -652,7 +653,7 @@ impl MigtdMessageHeader {
         if bytes.len() < size_of::<Self>() {
             return None;
         }
-        let header = MigtdMessageHeader {
+        let header = PreSessionMessage {
             r#type: bytes[0],
             reserved: bytes[1..4].try_into().unwrap(),
             length: u32::from_le_bytes(bytes[4..8].try_into().unwrap()),
@@ -662,29 +663,67 @@ impl MigtdMessageHeader {
 }
 
 #[cfg(feature = "policy_v2")]
-async fn send_policy<T: AsyncRead + AsyncWrite + Unpin>(
-    policy: &[u8],
-    transport: &mut T,
-) -> Result<()> {
-    let header = MigtdMessageHeader {
-        r#type: MigtdMessageHeader::EXCHANGE_POLICY_TYPE,
-        reserved: [0u8; 3],
-        length: policy.len() as u32,
-    };
+struct HelloPacketPayload {
+    magic_word: [u8; 4],
+    lowest_supported_version: u16,
+    highest_supported_version: u16,
+}
 
-    let mut sent = 0;
-    while sent < header.as_bytes().len() {
-        let n = transport
-            .write(header.as_bytes())
-            .await
-            .map_err(|_| MigrationResult::NetworkError)?;
-        sent += n;
+#[cfg(feature = "policy_v2")]
+impl HelloPacketPayload {
+    const HELLO_PACKET_PAYLOAD_SIZE: usize = 8;
+    const HELLO_PACKET_MAGIC_WORD: [u8; 4] = [b'M', b'G', b'T', b'D'];
+    const LOWEST_VERSION: u16 = 0x0100;
+    const HIGHEST_VERSION: u16 = 0x0100;
+
+    pub const fn new() -> Self {
+        Self {
+            magic_word: Self::HELLO_PACKET_MAGIC_WORD,
+            lowest_supported_version: Self::LOWEST_VERSION,
+            highest_supported_version: Self::HIGHEST_VERSION,
+        }
     }
 
-    sent = 0;
-    while sent < policy.len() {
+    pub fn as_bytes(&self) -> &[u8] {
+        unsafe { core::slice::from_raw_parts(self as *const Self as *const u8, size_of::<Self>()) }
+    }
+
+    pub fn read_from_bytes(bytes: &[u8]) -> Option<Self> {
+        if bytes.len() < size_of::<Self>() {
+            return None;
+        }
+        let payload = HelloPacketPayload {
+            magic_word: bytes[..4].try_into().unwrap(),
+            lowest_supported_version: u16::from_le_bytes(bytes[4..6].try_into().unwrap()),
+            highest_supported_version: u16::from_le_bytes(bytes[6..8].try_into().unwrap()),
+        };
+
+        if payload.magic_word != HelloPacketPayload::HELLO_PACKET_MAGIC_WORD {
+            return None;
+        }
+        Some(payload)
+    }
+
+    fn negotiate_supported_version(&self) -> Option<u16> {
+        let low = core::cmp::max(Self::LOWEST_VERSION, self.lowest_supported_version);
+        let high = core::cmp::min(Self::HIGHEST_VERSION, self.highest_supported_version);
+        if low > high {
+            None
+        } else {
+            Some(high)
+        }
+    }
+}
+
+#[cfg(feature = "policy_v2")]
+async fn send_pre_session_data<T: AsyncRead + AsyncWrite + Unpin>(
+    transport: &mut T,
+    data: &[u8],
+) -> Result<()> {
+    let mut sent = 0;
+    while sent < data.len() {
         let n = transport
-            .write(&policy[sent..])
+            .write(&data[sent..])
             .await
             .map_err(|_| MigrationResult::NetworkError)?;
         sent += n;
@@ -693,79 +732,139 @@ async fn send_policy<T: AsyncRead + AsyncWrite + Unpin>(
 }
 
 #[cfg(feature = "policy_v2")]
-async fn receive_policy<T: AsyncRead + AsyncWrite + Unpin>(transport: &mut T) -> Result<Vec<u8>> {
-    let mut header_buffer = [0u8; size_of::<MigtdMessageHeader>()];
-
+async fn receive_pre_session_data<T: AsyncRead + AsyncWrite + Unpin>(
+    transport: &mut T,
+    data: &mut [u8],
+) -> Result<()> {
     let mut recvd = 0;
-    while recvd < header_buffer.len() {
+    while recvd < data.len() {
         let n = transport
-            .read(&mut header_buffer[recvd..])
+            .read(&mut data[recvd..])
             .await
             .map_err(|_| MigrationResult::NetworkError)?;
         recvd += n;
     }
-
-    let header = MigtdMessageHeader::read_from_bytes(&header_buffer)
-        .ok_or(MigrationResult::InvalidParameter)?;
-    if header.r#type != MigtdMessageHeader::EXCHANGE_POLICY_TYPE {
-        return Err(MigrationResult::InvalidParameter);
-    }
-
-    let policy_size = header.length as usize;
-    let mut policy = vec![0u8; policy_size];
-    recvd = 0;
-    while recvd < policy_size {
-        let n = transport
-            .read(&mut policy[recvd..])
-            .await
-            .map_err(|_| MigrationResult::NetworkError)?;
-        recvd += n;
-    }
-    Ok(policy)
+    Ok(())
 }
 
 #[cfg(feature = "policy_v2")]
-async fn send_start_session_command<T: AsyncRead + AsyncWrite + Unpin>(
+async fn send_pre_session_data_packet<T: AsyncRead + AsyncWrite + Unpin>(
+    pre_session_data: &[u8],
     transport: &mut T,
 ) -> Result<()> {
-    let header = MigtdMessageHeader {
-        r#type: MigtdMessageHeader::START_SESSION_TYPE,
+    let header = PreSessionMessage {
+        r#type: PreSessionMessage::PRE_SESSION_DATA_TYPE,
+        reserved: [0u8; 3],
+        length: pre_session_data.len() as u32,
+    };
+
+    send_pre_session_data(transport, header.as_bytes()).await?;
+    send_pre_session_data(transport, pre_session_data).await
+}
+
+#[cfg(feature = "policy_v2")]
+async fn receive_pre_session_data_packet<T: AsyncRead + AsyncWrite + Unpin>(
+    transport: &mut T,
+) -> Result<Vec<u8>> {
+    let mut header_buffer = [0u8; size_of::<PreSessionMessage>()];
+    receive_pre_session_data(transport, &mut header_buffer).await?;
+
+    let header = PreSessionMessage::read_from_bytes(&header_buffer)
+        .ok_or(MigrationResult::InvalidParameter)?;
+    if header.r#type != PreSessionMessage::PRE_SESSION_DATA_TYPE {
+        return Err(MigrationResult::InvalidParameter);
+    }
+
+    let pre_session_data_payload_size = header.length as usize;
+    let mut pre_session_data_payload = vec![0u8; pre_session_data_payload_size];
+    receive_pre_session_data(transport, &mut pre_session_data_payload).await?;
+
+    Ok(pre_session_data_payload)
+}
+
+#[cfg(feature = "policy_v2")]
+async fn send_start_session_packet<T: AsyncRead + AsyncWrite + Unpin>(
+    transport: &mut T,
+) -> Result<()> {
+    let header = PreSessionMessage {
+        r#type: PreSessionMessage::START_SESSION_TYPE,
         reserved: [0u8; 3],
         length: 0,
     };
 
-    let mut sent = 0;
-    while sent < header.as_bytes().len() {
-        let n = transport
-            .write(header.as_bytes())
-            .await
-            .map_err(|_| MigrationResult::NetworkError)?;
-        sent += n;
+    send_pre_session_data(transport, header.as_bytes()).await
+}
+
+#[cfg(feature = "policy_v2")]
+async fn receive_start_session_packet<T: AsyncRead + AsyncWrite + Unpin>(
+    transport: &mut T,
+) -> Result<()> {
+    let mut header_buffer = [0u8; size_of::<PreSessionMessage>()];
+    receive_pre_session_data(transport, &mut header_buffer).await?;
+
+    let packet = PreSessionMessage::read_from_bytes(&header_buffer)
+        .ok_or(MigrationResult::InvalidParameter)?;
+
+    // Sanity checks
+    if packet.r#type != PreSessionMessage::START_SESSION_TYPE {
+        return Err(MigrationResult::InvalidParameter);
     }
+    if packet.length != 0 {
+        return Err(MigrationResult::InvalidParameter);
+    }
+
     Ok(())
 }
 
 #[cfg(feature = "policy_v2")]
-async fn receive_start_session_command<T: AsyncRead + AsyncWrite + Unpin>(
+async fn send_hello_packet<T: AsyncRead + AsyncWrite + Unpin>(transport: &mut T) -> Result<()> {
+    let header = PreSessionMessage {
+        r#type: PreSessionMessage::HELLO_PACKET_TYPE,
+        reserved: [0u8; 3],
+        length: 8,
+    };
+    send_pre_session_data(transport, header.as_bytes()).await?;
+
+    let payload = HelloPacketPayload::new();
+    send_pre_session_data(transport, payload.as_bytes()).await
+}
+
+#[cfg(feature = "policy_v2")]
+async fn receive_hello_packet<T: AsyncRead + AsyncWrite + Unpin>(
     transport: &mut T,
-) -> Result<()> {
-    let mut header_buffer = [0u8; size_of::<MigtdMessageHeader>()];
+) -> Result<HelloPacketPayload> {
+    let mut header_buffer = [0u8; size_of::<PreSessionMessage>()];
+    receive_pre_session_data(transport, &mut header_buffer).await?;
 
-    let mut recvd = 0;
-    while recvd < header_buffer.len() {
-        let n = transport
-            .read(&mut header_buffer[recvd..])
-            .await
-            .map_err(|_| MigrationResult::NetworkError)?;
-        recvd += n;
-    }
-
-    let command = MigtdMessageHeader::read_from_bytes(&header_buffer)
+    let header = PreSessionMessage::read_from_bytes(&header_buffer)
         .ok_or(MigrationResult::InvalidParameter)?;
-    if command.r#type != MigtdMessageHeader::START_SESSION_TYPE {
+
+    // Sanity checks
+    if header.r#type != PreSessionMessage::HELLO_PACKET_TYPE {
         return Err(MigrationResult::InvalidParameter);
     }
-    Ok(())
+    if header.length as usize != HelloPacketPayload::HELLO_PACKET_PAYLOAD_SIZE {
+        return Err(MigrationResult::InvalidParameter);
+    }
+
+    // Receive hello packet payload
+    let mut hello_payload = vec![0u8; HelloPacketPayload::HELLO_PACKET_PAYLOAD_SIZE];
+    receive_pre_session_data(transport, &mut hello_payload).await?;
+
+    HelloPacketPayload::read_from_bytes(&hello_payload).ok_or(MigrationResult::InvalidParameter)
+}
+
+// Exchange hello packet and negotiate a pre-session message version
+#[cfg(feature = "policy_v2")]
+async fn exchange_hello_packet<T: AsyncRead + AsyncWrite + Unpin>(
+    transport: &mut T,
+) -> Result<u16> {
+    send_hello_packet(transport).await?;
+    let remote = receive_hello_packet(transport).await?;
+
+    remote
+        .negotiate_supported_version()
+        .ok_or(MigrationResult::InvalidParameter)
 }
 
 #[cfg(feature = "policy_v2")]
@@ -774,12 +873,15 @@ async fn pre_session_data_exchange<T: AsyncRead + AsyncWrite + Unpin>(
 ) -> Result<Vec<u8>> {
     use crate::config;
 
-    let policy = config::get_policy().ok_or(MigrationResult::InvalidParameter)?;
-    send_policy(policy, transport).await?;
-    let remote_policy = receive_policy(transport).await?;
+    let version = exchange_hello_packet(transport).await?;
+    log::info!("Pre-Session-Message Version: 0x{:04x}\n", version);
 
-    send_start_session_command(transport).await?;
-    receive_start_session_command(transport).await?;
+    let policy = config::get_policy().ok_or(MigrationResult::InvalidParameter)?;
+    send_pre_session_data_packet(policy, transport).await?;
+    let remote_policy = receive_pre_session_data_packet(transport).await?;
+
+    send_start_session_packet(transport).await?;
+    receive_start_session_packet(transport).await?;
 
     Ok(remote_policy)
 }
